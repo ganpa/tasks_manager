571e536de090992704e81fc52d2215947deb386d
Ganapathy <ganapathy@freshdesk.com>
2016-03-05 16:37:58 +0530

    fixed location select logic

--- public/js/location_select_controller.js
+++ public/js/location_select_controller.js
@@ -5,6 +5,8 @@ app.controller('LocationSelectController', ['$scope', '$rootScope', 'LocationSer
     $scope.locations = [];
     $scope.location_ids = [];
     $scope.location_maps = [];
+    $scope.SELECT = "--Select--";
+    $scope.SELECT_ID = 0;
     LocationService.get_location_types(function(location_types){
       $scope.location_types = location_types;
       //$scope.location_type = $scope.location_types[0];
@@ -23,13 +25,26 @@ app.controller('LocationSelectController', ['$scope', '$rootScope', 'LocationSer
   };
 
   $scope.location_type_value_select = function(index, call_back){
+    console.log("index", index);
+    console.log("location_type-num", $scope.location_type_num, $scope.name);
     if(($scope.location_type_num == index+1 && $scope.name == "location") || 
-        $scope.location_type_num == index){
+      $scope.location_type_num == index){
       console.log("return");
       if(call_back != undefined){
         console.log("callback is defined");
         call_back();
       }
+      return;
+    }
+
+    if($scope.location_types_value[index] == $scope.SELECT){
+      console.log("resetting values");
+      for(i=index+1; i<= $scope.location_type_num; i++){
+        $scope.locations[i] = [];
+        $scope.location_types_value[i] = null;
+      }
+      if(call_back != undefined)
+        call_back();
       return;
     }
     
@@ -40,14 +55,19 @@ app.controller('LocationSelectController', ['$scope', '$rootScope', 'LocationSer
     console.log("location_type_value", parent_location_type_value);
     
     location_id = $scope.get_location_id(index);
-    for(i=index+1; i<$scope.location_type_num; i++){
-      $scope.locations[i] = ["--Select--"];
+    for(i=index+1; i< $scope.location_type_num; i++){
+      $scope.locations[i] = [$scope.SELECT];
     }
+    $scope.locations[$scope.location_type_num] = [];
     // location_id = $scope.location_maps[index][$scope.location_types_value[index]];
     //LocationService.get_location_by_type_and_parent(index, 
     //  parent_location_type, parent_location_type_value, $scope.location_types[index+1], 
     LocationService.get_sublocations(location_id, function(response){
         result = response.data[$scope.location_types[index+1]];
+        if(!($scope.location_type_num == index+1)){
+          result.unshift({"name" : $scope.SELECT, "id" : $scope.SELECT_ID});
+        }
+
         console.log("result", result);
         result_names = [];
         result_ids = [];
@@ -59,11 +79,15 @@ app.controller('LocationSelectController', ['$scope', '$rootScope', 'LocationSer
         }
 
         $scope.locations[index+1] = result_names;
-        $scope.locations[index+1].unshift("--Select--");
-        $scope.location_types_value[index+1] = "--Select--";
+        // $scope.locations[index+1].unshift("--Select--");
+        if(!($scope.location_type_num == index+1)){
+          $scope.location_types_value[index+1] = $scope.SELECT;
+        }
+        $scope.location_types_value[index+1] = $scope.SELECT;
         $scope.location_ids[index+1] = result_ids;
         $scope.location_maps[index+1] = location_map;
         console.log("location_maps", $scope.location_maps);
+        console.log("location_ids", $scope.location_ids);
 
         console.log($scope.locations[index+1]);
 
@@ -82,28 +106,37 @@ app.controller('LocationSelectController', ['$scope', '$rootScope', 'LocationSer
     console.log("Select");
     console.log($scope.location_type);
     $scope.location_type_num = $scope.location_types.indexOf($scope.location_type);
-    if(($scope.location_type_num > 0 && $scope.name=="location") || $scope.location_type_num >= 0){
+    if(($scope.location_type_num > 0 && $scope.name=="location") || 
+      ($scope.location_type_num >= 0 && $scope.name != "location")){
       // LocationService.get_location_by_type($scope.location_types[0], function(result){
       LocationService.get_sublocations($rootScope.account.location_id, function(response){
         result = response.data[$scope.location_types[0]];
-
+          
+        if(!($scope.location_type_num == 0)){
+          result.unshift({"name" : $scope.SELECT, "id" : $scope.SELECT_ID})
+        }
+        console.log("result", result);
         result_names = [];
         result_ids = [];
         location_map = {};
 
-        for(i=0; i<result.length; i++){
+        // for(i=0; i<result.length; i++){
+        (result || []).forEach(function(element, i, result){
           result_names[i] = result[i].name;
           result_ids[i] = result[i].id;
           location_map[result[i].name] = result[i].id
+        });
 
-        }
         $scope.locations[0] = result_names;  
-        $scope.locations[0].unshift("--Select--");
-        $scope.location_types_value[0] = "--Select--";
-
+        // $scope.locations[0].unshift("--Select--");
+        if(!($scope.location_type_num == 0)){
+          $scope.location_types_value[0] = $scope.SELECT;
+        }
         $scope.location_ids[0] = result_ids;
         $scope.location_maps[0] = location_map;
         console.log("location_maps", $scope.location_maps);
+        console.log("location_ids", $scope.location_ids);
+
       });
     }
     $scope.location_types_value = [];
